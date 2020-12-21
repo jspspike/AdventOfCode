f6357a1826cda7aa7755f5edb7bfcd7444f1534c
jspspike <jspspike@gmail.com>
2020-12-20 19:16:29 -0600

    Finished 08

--- 2020/08/game/src/main.rs
+++ 2020/08/game/src/main.rs
@@ -36,20 +36,16 @@ impl From<String> for Inst {
     }
 }
 
-fn main() {
-    let input = File::open("input").unwrap();
-
-    let lines: Vec<Inst> = BufReader::new(input)
-        .lines()
-        .filter_map(|x| x.ok())
-        .map(Inst::from)
-        .collect();
-
+fn run(lines: &[Inst]) -> Option<i32> {
     let mut index: i32 = 0;
     let mut acc = 0;
     let mut visited = HashSet::new();
 
     loop {
+        if index as usize >= lines.len() {
+            return Some(acc);
+        }
+
         let inst = lines[index as usize];
 
         match inst.op {
@@ -66,11 +62,39 @@ fn main() {
         };
 
         if visited.contains(&index) {
-            break;
+            return None;
         } else {
             visited.insert(index);
         }
     }
+}
 
-    println!("{}", acc);
+fn main() {
+    let input = File::open("input").unwrap();
+
+    let lines: Vec<Inst> = BufReader::new(input)
+        .lines()
+        .filter_map(|x| x.ok())
+        .map(Inst::from)
+        .collect();
+
+    for i in 0..lines.len() {
+        let line = lines[i];
+
+        if let Op::Acc = line.op {
+            continue;
+        }
+
+        let mut modified = lines.clone();
+        match line.op {
+            Op::Nop => modified[i].op = Op::Jmp,
+            Op::Jmp => modified[i].op = Op::Nop,
+            _ => {}
+        }
+
+        if let Some(acc) = run(&modified) {
+            println!("{}", acc);
+            return;
+        }
+    }
 }
